edd78fc578e3418138b8cc8c72c609ae336f630d
Kiup Kim <kukim96@gmail.com>
2021-04-20 17:46:55 -0600

    added character counter and updated color to red when below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  
+  // updating number of characters after input
+  $("#tweet-text").on("keyup", function() {
+    const numOfChar = $("#tweet-text").val();
+    console.log(numOfChar, numOfChar.length);
+
+    const tweetLength = 140 - numOfChar.length;
+    $("#tweet-text").parent().find("output").val(tweetLength);
+
+
+    if (tweetLength < 0) {
+      $("#tweet-text").parent().find('output').addClass('over-char-limit');
+    } else {
+      $("#tweet-text").parent().find('output').removeClass('over-char-limit');
+    }
+  })
+});
